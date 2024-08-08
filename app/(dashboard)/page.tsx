2534b83c1d9710cfd17204cbde136ad21df46777
Satria Sapta <satriasapta48@gmail.com>
2024-08-08 22:22:23 +0700

    accounts get API

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,10 +1,12 @@
-import { UserButton } from "@clerk/nextjs";
-import { User } from "lucide-react";
+"use client"
+
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
 
 export default function Home() {
+  const accountsQuery = useGetAccounts();
   return (
     <div>
-      Dashboard Page
+      Home Page
     </div>
-  );
+  )
 }
